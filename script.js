5b9cea674d7bb4463000cfca194a87c001be896d
Shweta Shaw <189984154+ShwetaShaw76@users.noreply.github.com>
2025-06-25 11:44:04 +0000

    added some changes

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 nav=document.querySelector(".nav-links")
+nxt=document.querySelector(".next")
+prev=document.querySelector(".prev")
+img=document.querySelectorAll(".img")
+post=document.querySelector(".post")
 
+
+let i = 0; 
+
+nxt.addEventListener("click", function () {
+    if (i < img.length - 1) {
+        img[i].classList.add("hidden");
+        i++;
+        img[i].classList.remove("hidden");
+
+        
+        prev.classList.remove("hidden");
+    }
+
+    if (i === img.length - 1) {
+        nxt.classList.add("hidden");
+    }
+});
+
+prev.addEventListener("click", function () {
+    if (i > 0) {
+        img[i].classList.add("hidden");
+        i--;
+        img[i].classList.remove("hidden");
+        nxt.classList.remove("hidden");
+    }
+
+    if (i === 0) {
+        prev.classList.add("hidden");
+    }
+});
 
 function onToggleMenu(e){
     e.name = e.name === "menu" ? "close" : "menu"
